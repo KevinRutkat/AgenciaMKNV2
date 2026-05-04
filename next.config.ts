41d38c4ea3c47e75891db32927f633ed6e523289
KevinRutkat <negotkat@gmail.com>
2026-05-04 17:53:10 +0200

    cambios de vercel optimization

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 // Configuración de Next.js - aquí definimos cómo se comporta nuestra aplicación
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
